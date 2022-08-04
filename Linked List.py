f4ee8753c8354efdce298b9d1235c60d1b2f2db2
gauthamcity <vjosh53@hotmail.com>
2022-08-04 12:25:26 -0600

    Update Linked List.py

--- Linked List.py
+++ Linked List.py
@@ -10,3 +10,22 @@ class LinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
